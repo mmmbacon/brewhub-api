abf33fe72124e08fba342f89056fd173a1e04da5
Brandon Macdonald <bmacdonald1986@gmail.com>
2021-08-11 23:53:04 -0600

    Add endpoint for filtering by city, some JSDoc comments

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const Sequelize = require('sequelize');
 
 const app = express();
 const port = 5000;
@@ -13,7 +14,25 @@ app.listen(process.env.PORT || port, () => {
   console.log('Server is listening');
 });
 
+/**
+ * Endpoint for fetching all breweries
+ */
 app.get('/breweries/all', async (req, res) => {
   const result = await queryModel.findAll();
+  res.json(result);
+});
+
+/**
+ * Endpoint for fetching breweries by city
+ */
+app.get('/breweries/cities/:city', async (req, res) => {
+  const { city } = req.params;
+  const result = await queryModel.findAll({
+    where: {
+      city: {
+        [Sequelize.Op.iLike]: city,
+      },
+    },
+  });
   res.json(result);
 });
